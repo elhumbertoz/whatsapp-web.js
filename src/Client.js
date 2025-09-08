173f0fbec09734563510d62023ce670573221f3f
Humberto Zambrano <elhumbertoz@gmail.com>
2025-09-08 10:03:44 -0500

    fix: add delay before evaluating ExposeStore to ensure proper module readiness

--- src/Client.js
+++ src/Client.js
@@ -218,6 +218,7 @@ class Client extends EventEmitter {
                 }
 
                 if (isCometOrAbove) {
+                    await new Promise(r => setTimeout(r, 10000));
                     await this.pupPage.evaluate(ExposeStore);
                 } else {
                     // make sure all modules are ready before injection
